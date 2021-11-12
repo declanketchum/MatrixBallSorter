cca2cf8ae9d4909232f072cd2a732dbf8a5fad0a
Declan Ketchum <dketchum@olin.edu>
2021-11-11 23:17:44 -0500

    playing around with html

--- readImage.py
+++ readImage.py
@@ -1,16 +1,19 @@
 from PIL import Image
 import numpy as np
 
-img = Image.open('olin_o.jpeg')
+img = Image.open("logo.png")
+img.show()
 
 width = 20
 height = 20
 
 img1 = img.resize((width,height))
 print(img1.size)
+img1.show()
 
 img2 = img1.convert("1")
+print(img2)
 img2.show()
-
-pix = np.array(img2)
-print(pix)
+#
+# pix = np.array(img2)
+# print(pix)
