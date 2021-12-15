843cacb0478cc6164ee7063ce4e2901d07b6bd3c
Meagan Martin <mmartin1@olin.edu>
2021-12-14 22:50:36 -0500

    updated

--- Final Website/assets/js/main.js
+++ Final Website/assets/js/main.js
@@ -64,6 +64,35 @@
 				target: $body,
 				visibleClass: 'is-menu-visible'
 			});
+	// Slideshow
+		var slideIndex = 1;
+		showSlides(slideIndex);
+
+	// Next/previous controls
+		function plusSlides(n) {
+  			showSlides(slideIndex += n);
+		}
+
+	// Thumbnail image controls
+		function currentSlide(n) {
+  			showSlides(slideIndex = n);
+		}
+
+		function showSlides(n) {
+  			var i;
+  			var slides = document.getElementsByClassName("mySlides");
+  			var dots = document.getElementsByClassName("dot");
+  			if (n > slides.length) {slideIndex = 1}
+ 			if (n < 1) {slideIndex = slides.length}
+  			for (i = 0; i < slides.length; i++) {
+      		slides[i].style.display = "none";
+  			}
+  			for (i = 0; i < dots.length; i++) {
+      			dots[i].className = dots[i].className.replace(" active", "");
+ 			 }
+ 			slides[slideIndex-1].style.display = "block";
+ 			 dots[slideIndex-1].className += " active";
+		}
 
 	// Header.
 		if ($banner.length > 0
@@ -79,34 +108,4 @@
 			});
 
 		}
-	// Slideshow
-	var slideIndex = 1;
-	showSlides(slideIndex);
-
-	// Next/previous controls
-	function plusSlides(n) {
-  showSlides(slideIndex += n);
-	}
-
-	// Thumbnail image controls
-	function currentSlide(n) {
-  	showSlides(slideIndex = n);
-	}
-
-	function showSlides(n) {
-  	var i;
-  	var slides = document.getElementsByClassName("mySlides");
-  	var dots = document.getElementsByClassName("dot");
-  	if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  	for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  	}
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
 })(jQuery);
